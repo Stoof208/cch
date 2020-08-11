5e31a2415ac86f78c14d8406051b87755e277d8e
Stoof208 <69509106+Stoof208@users.noreply.github.com>
2020-08-11 19:53:57 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+CHARSET = '''0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()`~-=_+[]\{}|;':",./<>? '''
+
+def idontknowwhattonamethisfunction(length, inputs):
+    a = [0] * length
+    b = [CHARSET.index(c) for c in ''.join(inputs)]
+    ba = len(b)
+    d = len(CHARSET)
+    c = ((sum(b) ** 2) + (length ** 3)) % d
+    for i in range(max(ba, length)):
+        bc = b[i % ba]
+        a[i % length] += (bc + c)
+        c += (bc ** 2) % d
+    return ''.join([char.CHARSET[cc % d] for cc in a])
